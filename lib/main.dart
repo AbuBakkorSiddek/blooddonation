27b1e49ad168c4426042a7e13e72d247f3aa1197
SIDDEK <siddek7277@gmail.com>
2022-02-14 02:11:59 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 
-import 'package:blooddonation/page/DashBoard.dart';
 import 'package:blooddonation/page/slashScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
